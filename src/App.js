c79c627e31e3d768a07751987efe5c1273bd8571
4bdeladim <abdeladim.frontend@gmail.com>
2020-11-01 08:31:44 +0100

    try to fix the problem

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react'
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './Pages';
+import SigninPage from './Pages/signin';
 
 function App() {
   return (
     <Router >
-     <Home /> 
+      <Switch>
+        <Route path="/Web-site" component={Home} exact />
+        <Route path="/signin" component={SigninPage} />
+      </Switch> 
     </Router>
   );
 }
